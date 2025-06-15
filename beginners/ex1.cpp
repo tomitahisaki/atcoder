7faf36285092de2922e10d79b0d38aaa972277b5
hisakey <alltimelow2011@gmail.com>
2025-06-15 20:55:33 +0900

    ex1 ex2

---
+++ beginners/ex1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+// こんにちは
+// AtCoder
+int main() {
+  cout << "こんにちは" << endl;
+  cout << "AtCoder" << endl;
+  return 0;
+}
---
+++ beginners/ex2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "いつも" << 2525 << endl;
+  cout << "AtCoderくん" << endl;
+  return 0;
+}
