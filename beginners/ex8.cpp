4e66b3976244e70a335ae88af925da1befcfc936
hisakey <alltimelow2011@gmail.com>
2025-06-18 06:11:59 +0900

    feat: ex8 ex9

---
+++ beginners/ex8.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int p;
+  cin >> p;
+
+  // パターン1
+  if (p == 1) {
+    int price;
+    int N;
+    cin >> price >> N;
+    cout << price * N << endl;
+    return 0;
+  }
+
+  // パターン2
+  if (p == 2) {
+    string text;
+    int price;
+    int N;
+    cin >> text >> price >> N;
+    cout << text << "!" << endl;
+    cout << price * N << endl;
+    return 0;
+  }
+}
+
