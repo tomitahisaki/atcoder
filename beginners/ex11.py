8774d1ffe52e2a9bd4d775a2172a602e6c988631
hisakey <alltimelow2011@gmail.com>
2025-07-05 21:57:37 +0900

    ex11

---
+++ beginners/ex11.py
@@ -0,0 +1,14 @@
+S = input()
+# ここにプログラムを追記
+
+resuilt = 1
+
+for i in range(len(S)):
+  if S[i] == "+":
+    resuilt += 1
+  elif S[i] == "-":
+    resuilt -= 1
+  else:
+    continue
+
+print(resuilt)
