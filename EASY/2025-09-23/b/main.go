1e00405c671201a4cf60e285003bd02be468f300
hisakey <hogehoge@example.com>
2025-09-25 05:45:24 +0900

    fix: b

--- EASY/2025-09-23/b/main.go
+++ EASY/2025-09-23/b/main.go
@@ -6,25 +6,16 @@ func main() {
 	var arr [8]int
 	fmt.Scan(&arr[0], &arr[1], &arr[2], &arr[3], &arr[4], &arr[5], &arr[6], &arr[7])
 
-	result := true
-
 	for i := range arr[:7] {
-		if arr[i] >= arr[i+1] {
-			result = false
+		if arr[i] < 100 || 675 < arr[i] || arr[i]%25 != 0 {
+			fmt.Println("No")
+			return
 		}
 
-		if arr[i] < 100 || 675 < arr[i] {
-			result = false
-		}
-
-		if arr[i]%25 != 0 {
-			result = false
+		if arr[i] > arr[i+1] {
+			fmt.Println("No")
+			return
 		}
 	}
-
-	if result {
-		fmt.Println("Yes")
-	} else {
-		fmt.Println("No")
-	}
+	fmt.Println("Yes")
 }
